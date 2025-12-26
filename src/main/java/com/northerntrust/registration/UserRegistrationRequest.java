7e9be2e5588cdf15821739160e3071c2943ecafc
Ramesh Appat <rameshappat@gmail.com>
2025-12-25 17:31:43 -0700

    feat: add UserRegistrationRequest.java

---
+++ src/main/java/com/northerntrust/registration/UserRegistrationRequest.java
@@ -0,0 +1,31 @@
+package com.northerntrust.registration;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+public class UserRegistrationRequest {
+
+    @NotBlank(message = "Email is mandatory")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @NotBlank(message = "Password is mandatory")
+    private String password;
+
+    // Getters and Setters
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
